9602ea661f236dd30c811fb73743debaf8078580
Gregory Newton <gregory_newton@outlook.com>
2018-09-12 23:14:35 -0600

    Converted min and min input to ints

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from random import randint
-
-min = input("What is the lowest number on your dice?")
-
-max = input("What is the highest number on your dice?")
-
-keepPlaying = True;
-
 def main():
-    while (keepPlaying == True):
-        print("Your dice rolled a: " + randint(min, max))
+    from random import randint
 
-        keepPlaying = input("Do you want to play again (Type 'Yes' or 'No'?")
+    min = int(input("What is the lowest number on your dice?  "))
+
+    max = int(input("What is the highest number on your dice?  "))
+
+    keepPlaying = "Yes";
+
+    while (keepPlaying == "Yes"):
+        print("Your dice rolled a: " + str(randint(min, max)))
+
+        keepPlaying = input("Do you want to play again (type 'Yes' or 'No')?  ")
 
 if __name__ == "__main__":
     main()
